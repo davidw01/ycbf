6dbd14955c7208551ff1402170d7c11261a6a320
David Weller <davidweller@Davids-MBP-4.wireless.yale.internal>
2017-11-16 16:54:13 -0500

    port fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5432;
 
 app.engine('handlebars', exphbs({
   defaultLayout: 'main',
